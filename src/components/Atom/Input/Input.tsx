dea7ad6d7bd12839c983772f7c45d815844def2f
Tran Hoang Phuc <phucth@haibazo.com>
2025-10-09 16:16:40 +0700

    feat(ITS-IR01-146): adding set up component

---
+++ src/components/Atom/Input/Input.tsx
@@ -0,0 +1,11 @@
+export const Input = () => {
+  return (
+    <div className="">
+      <label htmlFor="test">lol</label>
+      <br />
+      <input id="test" type="text" />
+    </div>
+  );
+};
+
+export default Input;
--- src/pages/HBZ0000/HBZ0000.tsx
+++ src/pages/HBZ0000/HBZ0000.tsx
@@ -3,6 +3,7 @@ import { Logo } from '@components/Atom/Logo/Logo';
 import Toggle from '@components/Atom/Toggle/Toggle';
 import { memo } from 'react';
 import { Button } from '../../components/Atom/Button/Button';
+import { Input } from '../../components/Atom/Input/Input';
 
 const HBZ0000Component = () => {
   return (
@@ -14,6 +15,8 @@ const HBZ0000Component = () => {
       <Link href="#">Hello from HAIBAZO ^_^</Link>
       <Toggle />
       <Logo size="large" logoName="PumaLogo" />
+
+      <Input />
     </>
   );
 };
