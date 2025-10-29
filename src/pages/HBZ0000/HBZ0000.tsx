a300a1cfe57c666f9302ac16ef5e2dddd835f54f
Tran Hoang Phuc <phucth@haibazo.com>
2025-10-29 14:16:16 +0700

    feat(ITS-IR01-182): add border radius for section component

--- src/pages/HBZ0000/HBZ0000.tsx
+++ src/pages/HBZ0000/HBZ0000.tsx
@@ -1,6 +1,8 @@
+import { Flex } from '@components/Atom/Flex';
 import { Link } from '@components/Atom/Link';
 import { Logo } from '@components/Atom/Logo/Logo';
 import { Section } from '@components/Atom/Section';
+import { Text } from '@components/Atom/Text';
 import Toggle from '@components/Atom/Toggle/Toggle';
 import { memo, useState } from 'react';
 import { Button } from '../../components/Atom/Button/Button';
@@ -60,7 +62,13 @@ const HBZ0000Component = () => {
           <Slider slides={sliderSlides} width={200} autoPlay={3000} loop={true} />
         </div>
       </div>
-      <Section></Section>
+      <Section w={20} bgColor="black" h={20} borderRadius="100%">
+        <Flex direction="column" align="center">
+          <Text font="inter" color="white" weight="bold" size="xsmall">
+            2
+          </Text>
+        </Flex>
+      </Section>
     </>
   );
 };
