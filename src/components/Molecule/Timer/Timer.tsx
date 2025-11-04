d56c6f01b923dd78b5c8b65310e66cb20518dc1f
Tran Hoang Phuc <phucth@haibazo.com>
2025-11-04 15:48:08 +0700

    feat(ITS-IR01-175): component, stories and test finsihed for TImer molecule

---
+++ src/components/Molecule/Timer/Timer.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import { labelCva, labelSpanCva, timeBlockCva, titleCva } from './style';
+import { TimerProps } from './type';
+
+export const Timer = ({ endDate, start, label, mobile, labelSpan, ...props }: TimerProps) => {
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    if (!endDate || !start) return;
+
+    const [day, month, year] = endDate.split('/').map(Number);
+    const end = new Date(year, month - 1, day).getTime();
+    const interval = setInterval(() => {
+      const now = Date.now();
+      const distance = end - now;
+
+      if (distance <= 0) {
+        clearInterval(interval);
+        setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      } else {
+        setTimeLeft({
+          days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((distance / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((distance / (1000 * 60)) % 60),
+          seconds: Math.floor((distance / 1000) % 60),
+        });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [endDate, start]);
+
+  return (
+    <div className="flex h-fit w-fit flex-col gap-[12px]" {...props}>
+      <div className={labelCva({ mobile })}>
+        {label}
+        <span className={labelSpanCva({ mobile })}>{labelSpan}</span>
+      </div>
+      <div className="flex gap-[16px] text-center">
+        {[
+          { label: 'Days', value: timeLeft.days },
+          { label: 'Hours', value: timeLeft.hours },
+          { label: 'Minutes', value: timeLeft.minutes },
+          { label: 'Seconds', value: timeLeft.seconds },
+        ].map((item) => (
+          <div
+            key={item.label}
+            className={`flex flex-col items-center justify-center ${mobile ?? 'gap-[2px]'} `}
+          >
+            <span className={timeBlockCva({ mobile })}>{String(item.value).padStart(2, '00')}</span>
+            <span className={titleCva({ mobile })}>{item.label}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
