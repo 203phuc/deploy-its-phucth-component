0d06bb7855db197853db04f690dfeb683b897d36
Tran Hoang Phuc <phucth@haibazo.com>
2025-12-02 11:50:20 +0700

    feat(ITS-IR01-258): new stories file

--- src/pages/ForgotPasspage/ForgotPassPage.stories.tsx
+++ src/pages/ForgotPasspage/ForgotPassPage.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { ForgotPassPage } from './ForgotPasspage';
+import { ForgotPassPage } from './ForgotPassPage';
 
 const meta: Meta<typeof ForgotPassPage> = {
   title: 'Page/ForgotPassPage',
