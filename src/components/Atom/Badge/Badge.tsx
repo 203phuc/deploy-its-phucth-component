77d788f647cd81d5338a44db78024b628d2f52ce
Tran Hoang Phuc <phucth@haibazo.com>
2025-12-15 09:06:17 +0700

    fix(ITS-IR01-282): fix add new circle variants

--- src/components/Atom/Badge/Badge.tsx
+++ src/components/Atom/Badge/Badge.tsx
@@ -2,10 +2,13 @@ import { cn } from '../../../util/tailwindClass';
 import { badgeCva } from './style';
 import type { BadgeProps } from './type';
 
-export const Badge = ({ size, color, roundness, variant, className, ...props }: BadgeProps) => {
+export const Badge = ({ size, color, roundness, variant, className, theme, ...props }: BadgeProps) => {
   const finalColor = variant === 'outline' ? 'none' : color;
 
   return (
-    <span className={cn(badgeCva({ size, color: finalColor, roundness, variant }), className)} {...props} />
+    <span
+      className={cn(badgeCva({ size, theme, color: finalColor, roundness, variant }), className)}
+      {...props}
+    />
   );
 };
--- src/components/Atom/Badge/style.ts
+++ src/components/Atom/Badge/style.ts
@@ -6,10 +6,18 @@ export const badgeCva = cva('inline-flex items-center justify-center', {
       outline: 'border-[1.5px] border-current bg-transparent',
       solid: '', // use custom class for color background if needed
     },
+    theme: {
+      whiteSolid: 'bg-[#FFFFFF] text-black-900',
+      blackSolid: 'bg-[#121212] text-white',
+      whiteOutline: 'border-[#FFFFFF] text-white',
+    },
     size: {
       small: 'gap-[4px] px-[10px] py-[2px] text-[12px] leading-[20px] font-[600]',
       medium: 'gap-[4px] px-[14px] py-[3px] text-[14px] leading-[24px] font-[600]',
       large: 'gap-[4px] px-[16px] py-[4px] text-[16px] leading-[26px] font-[600]',
+      circleLarge: 'h-[65px] w-[65px] text-[16px] leading-[26px]',
+      circleMedium: 'h-[60px] w-[60px] text-[14px] leading-[22px]',
+      circleSmall: 'h-[45px] w-[45px] text-[12px] leading-5',
     },
     color: {
       red: 'bg-red-500 text-white',
