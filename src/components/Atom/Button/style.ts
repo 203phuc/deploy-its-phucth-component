df84d10fe841d2faa8d529f6c95b8da001408db8
Tran Hoang Phuc <phucth@haibazo.com>
2025-12-15 09:38:19 +0700

    fix(ITS-IR01-299): fix button size

--- src/components/Atom/Button/style.ts
+++ src/components/Atom/Button/style.ts
@@ -15,12 +15,12 @@ export const buttonCva = cva(
       },
 
       size: {
-        xlarge: 'h-[76px] min-w-[190px] gap-[8px] px-[55px] py-[19px] text-[26px] leading-[38px]',
-        large: 'h-[72px] min-w-[148px] gap-[8px] px-[44px] py-[19px] text-[22px] leading-[34px]',
-        largeCompact: 'h-[53px] min-w-[117px] gap-[8px] px-[44px] py-[19px] text-[22px] leading-[34px]',
-        medium: 'h-[52px] min-w-[119px] gap-[4px] px-[26px] py-[10px] text-[18px] leading-[32px]',
-        small: 'h-[46px] min-w-[88px] gap-[4px] px-[24px] py-[9px] text-[16px] leading-[28px]',
-        xsmall: 'h-[40px] min-w-[84px] gap-[2px] px-[20px] py-[8px] text-[14px] leading-[24px]',
+        xlarge: 'h-[76px] gap-[8px] px-[55px] py-[19px] text-[26px] leading-[38px]',
+        large: 'h-[72px] gap-[8px] px-[44px] py-[19px] text-[22px] leading-[34px]',
+        largeCompact: 'h-[53px] gap-[8px] px-[44px] py-[19px] text-[22px] leading-[34px]',
+        medium: 'h-[52px] gap-[4px] px-[26px] py-[10px] text-[18px] leading-[32px]',
+        small: 'h-[46px] gap-[4px] px-[24px] py-[9px] text-[16px] leading-[28px]',
+        xsmall: 'h-[40px] gap-[2px] px-[20px] py-[8px] text-[14px] leading-[24px]',
       },
 
       roundness: {
