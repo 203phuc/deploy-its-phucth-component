c0154a88dd96f8bca081078683cc7f731cf0e440
Tran Hoang Phuc <phucth@haibazo.com>
2025-12-18 14:15:34 +0700

    fix(ITS-IR01-313): fix disable issues

--- src/components/Molecule/Dropdown/Dropdown.stories.tsx
+++ src/components/Molecule/Dropdown/Dropdown.stories.tsx
@@ -60,22 +60,6 @@ const meta = {
         '4xlarge',
       ],
     },
-    textSize: {
-      control: 'select',
-      options: [
-        'special2',
-        'special1',
-        'xsmall',
-        'small',
-        'smedium',
-        'medium',
-        'large',
-        'xlarge',
-        '2xlarge',
-        '3xlarge',
-        '4xlarge',
-      ],
-    },
     value: {
       control: 'text',
       description: 'Currently selected value',
