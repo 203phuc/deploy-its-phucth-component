f616f8ddd122991c6097d5a63ad80bbb591fb955
Tran Hoang Phuc <phucth@haibazo.com>
2025-12-23 09:02:36 +0700

    feat(ITS-IR01-317): notification bar for shoppage

--- src/pages/Shoppage/ShopPage.tsx
+++ src/pages/Shoppage/ShopPage.tsx
@@ -1,13 +1,25 @@
 import { Flex } from '@components/Atom/Flex';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { onSmallScreenChange } from 'src/util/mediaQueries';
 import { Footer } from '../Homepage/sections/Footer';
+import { useHomePage } from '../Homepage/sections/hooks/HomePageHook';
 import { NavigationBar } from '../Homepage/sections/NavigationBar';
 import { PageHeader } from './components/PageHeader';
 
 export const ShopPage = () => {
+  const [isMobile, setIsMobile] = React.useState(false);
+  const { notificationHeight, scrolled } = useHomePage();
+  useEffect(() => {
+    return onSmallScreenChange(setIsMobile);
+  }, []);
   return (
     <React.Fragment>
-      <NavigationBar isMobile={false} />
+      <NavigationBar
+        isMobile={isMobile}
+        scrolled={scrolled}
+        translateY={notificationHeight}
+        transition="transform 220ms cubic-bezier(.2,.9,.2,1)"
+      />
       <Flex direction="column" align="center">
         <Flex flex={1} direction="column"></Flex>
         <PageHeader />
