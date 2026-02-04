36eba368bf51150d9035128e049271bad3bccd65
Tran Hoang Phuc <phucth@haibazo.com>
2026-02-04 15:09:00 +0700

    feat(ITS-IR01-408): fixing indexing

--- src/pages/HOC/Layout/index.tsx
+++ src/pages/HOC/Layout/index.tsx
@@ -113,10 +113,10 @@ function AppContent() {
         onClose={closeMessageModal}
         autoCloseDuration={5000}
       />
-      <Overlay isOpen={FlyoutMenuOpen} onClose={() => setFlyoutMenuOpen(false)} zIndex={100} position="left">
+      <Overlay isOpen={FlyoutMenuOpen} onClose={() => setFlyoutMenuOpen(false)} zIndex={101} position="left">
         <FlyoutMenu setFlyoutMenuOpen={setFlyoutMenuOpen} cartItem={2} />
       </Overlay>
-      <Overlay isOpen={flyoutCartOpen} onClose={() => setFlyoutCartOpen(false)} zIndex={10} position="right">
+      <Overlay isOpen={flyoutCartOpen} onClose={() => setFlyoutCartOpen(false)} zIndex={101} position="right">
         <FlyoutCart setFlyoutCartOpen={setFlyoutCartOpen} isMobile={isSmallScreen} />
       </Overlay>
 
